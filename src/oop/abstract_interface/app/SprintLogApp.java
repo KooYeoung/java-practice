8480c9da80597468628a215644e90312705a3b33
rndud <rndud008@gmail.com>
2026-05-11 13:11:44 +0900

    Java 객체지향 (SOLID) - SOLID 원칙 적용해 보기 - SRP: printer 관련 클래스 생성해서 출력 책임을 부여 - OCP: printer 역할을 부여하는 interface를 선언하여 확장에는 열려있고, 변경에는 닫힌 구조를 구축 - LSP: 자식은 부모 대신 사용되어도 정상적으로 동작됨을 보장해야 한다. - ISP: 인터페이스 분리 원칙(인터페이스는 작게, 역할별로 나눠야 필요한 역할만 골라서 구현할 수 있다.)

--- src/oop/abstract_interface/app/SprintLogApp.java
+++ src/oop/abstract_interface/app/SprintLogApp.java
@@ -6,6 +6,9 @@ import oop.abstract_interface.domain.PracticeLog;
 import oop.abstract_interface.domain.ReadingLog;
 import oop.abstract_interface.policy.Reviewable;
 import oop.abstract_interface.policy.Shareable;
+import oop.abstract_interface.printer.ActivityPrinter;
+import oop.abstract_interface.printer.CompactActivityPrinter;
+import oop.abstract_interface.printer.ConsoleActivityPrinter;
 
 public class SprintLogApp {
 
@@ -19,11 +22,19 @@ public class SprintLogApp {
       // 추상클래스는 스스로의 객체를 생성할 수 없고, 자식에 의해서 생성될 뿐입니다.
 //        LearningActivity act = new LearningActivity("test", 10);
 
+      //LSP - 부모타입의 자리에 자식이 들어가도 기능은 동일하게 동작함.
       LearningActivity[] activities = {javaLecture, gitPractice, oopPractice, oopBook};
+      ActivityPrinter printer = new ConsoleActivityPrinter();
+      ActivityPrinter compactPrinter = new CompactActivityPrinter();
 
       System.out.println("=== 학습 활동 목록 ===");
       for (int i = 0; i < activities.length; i++) {
-         activities[i].printSummary();
+         // SRP - 단일 책임 원칙으로 출력에대한 기능을 분리
+         // 출력방식을 변경하게 될경우 각 객체들에 대한 변경에대한 문제가 있었음
+         printer.print(activities[i]);
+         // OCP - 기능 확장에는 열려 있으나 변경에는 닫혀 있어야 한다.
+         // 출력형식에 대한 확장으로 구현체를 하나더 생성함.
+         compactPrinter.print(activities[i]);
       }
 
       // 다형성이란 부모 타입의 변수에 자식 타입의 객체가 들어올 수 있다는 것. 곧 상속 관계 하에서만 발생한다.
---
+++ src/oop/abstract_interface/printer/ActivityPrinter.java
@@ -0,0 +1,7 @@
+package oop.abstract_interface.printer;
+
+import oop.abstract_interface.domain.LearningActivity;
+
+public interface ActivityPrinter {
+   void print(LearningActivity activity);
+}
