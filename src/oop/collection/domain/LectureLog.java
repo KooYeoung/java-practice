21e33766ecc54daa87aa786df2ff8fc54c233e93
rndud <rndud008@gmail.com>
2026-05-12 17:07:33 +0900

    Java Collection - LearningActivity에 태그를 저장하기 위한 HashSet 생성 - ActivityDashboard의 배열을 List로 변경 - 필터링 로직 추가 (filterByTag(), groupByCategory())

---
+++ src/oop/collection/domain/LectureLog.java
@@ -0,0 +1,61 @@
+package oop.collection.domain;
+
+import oop.collection.policy.Reviewable;
+import oop.collection.policy.Shareable;
+
+// LectureLog는 LearningActivity의 한 종류이고, Reviewable에 선언된 역할도 수행할 수 있다.
+public class LectureLog extends LearningActivity implements Reviewable, Shareable {
+
+   public String instructorName; // 강사 이름 (LectureLog만 가지는 고유한 필드)
+
+   public LectureLog(String title, int minutes, Visibility visibility, String instructorName) {
+      super(title, minutes, visibility, ActivityCategory.LECTURE);
+      this.instructorName = normalizeInstructorName(instructorName);
+   }
+
+
+
+   @Override
+   public boolean needsReview() {
+      return getCategory().isShortStudy(getMinutes());
+   }
+
+   @Override
+   public void printReviewTarget() {
+      System.out.println("[복습 권장]" + getTitle() + " ("+ getMinutes() + "분)");
+   }
+
+   private String normalizeInstructorName(String instructorName) {
+      if (instructorName == null || instructorName.isBlank()) {
+         return "강사 미정";
+      }
+
+      return instructorName;
+   }
+
+
+   @Override
+   public boolean canShare() {
+      return isPublicActivity();
+   }
+
+   @Override
+   public String getShareTitle() {
+      return getTitle();
+   }
+
+   @Override
+   public String getActivityType() {
+      return getCategory().getLabel();
+   }
+
+   @Override
+   public String getDetailText() {
+      return "강사: " + instructorName;
+   }
+}
+
+
+
+
+
